a2577ac8e7d772e6dbb9efe9992a164c428097c5
Emati Mitame <GitCommits@mita.me>
2016-01-28 19:09:20 +0000

    Add the rest

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from flask import Flask, redirect, safe_join
+from flask import Flask, redirect, safe_join, request, url_for, jsonify
 from pymongo import MongoClient
+import time
 
 db = MongoClient()["shrt"]
 
 links = db["links"]
-files = db["files"]
 last_gen = db["last_gen"]
 
 app = Flask(__name__)
 
-LINK_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+LINK_CHARS = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+LINK_MIN_LENGTH = 4
 
 def set_base(num, base):
     units = [1]
@@ -33,10 +34,10 @@ def set_base(num, base):
 def get_code():
     data = last_gen.find_one({"obj": "link"})
     if data is None:
-        num = 0
+        num = len(LINK_CHARS) ** (LINK_MIN_LENGTH - 1)
         last_gen.insert_one({"obj": "link", "num": num})
     else:
-        num = data["num"] + 1
+        num = max((data["num"] + 1, len(LINK_CHARS) ** (LINK_MIN_LENGTH-1)))
 
     last_gen.update({"obj": "link"}, {"$set": {"num": num}})
 
@@ -46,17 +47,61 @@ def get_code():
         code += LINK_CHARS[x]
     return code
 
+def get_id(code):
+    code = str(code)[::-1]
+    num = 0
+    for c in range(len(code)):
+        num += LINK_CHARS.index(code[c])**(c+1)
+
+    return num
+
+
+
 @app.route("/api/mk", methods=["POST"])
 def mk_ln():
-    pass
+    url = request.form["url"]
+    code = get_code()
+    data = {
+        "type": "url",
+        "code": code,
+        "url": url,
+        "ts": str(time.time()),
+        "clicked": 0
+    }
+
+    links.insert_one(data)
+    return jsonify({
+        "ok": True,
+        "url": url_for("home", _external=True, _scheme="https") + code,
+    })
+
 
 @app.route("/")
 def home():
     return redirect("https://mita.me/")
 
+
+@app.route("/new")
+def new():
+    return get_code()
+
+
 @app.route("/<string:code>")
 def link(code):
-    return code
+    data = links.find_one({"code": code})
+    if data is not None:
+        links.update(
+            {
+                "_id": data["_id"]
+            },
+            {"$inc":
+                {"clicked": 1}
+            }
+        )
+        return redirect(data["url"])
+    else:
+        return home()
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
